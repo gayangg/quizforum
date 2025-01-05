d034a0c8d1ca8b12467fc5ce8a39c94db078f7cf
gayangg <gayang.fi@gmail.com>
2025-01-05 15:06:53 +0200

    remove localhost

--- run-locally.js
+++ run-locally.js
@@ -1,10 +1,10 @@
 import { app } from './app.js';
 
-const HOST = '127.0.0.1';
+//const HOST = '127.0.0.1';
 const PORT = Deno.env.get("PORT") || 7777;
 
-app.addEventListener('listen', ({ hostname, port }) => {
-  console.log(`Server running on http://${hostname}:${port}/`);
-});
+// app.addEventListener('listen', ({ hostname, port }) => {
+//   console.log(`Server running on http://${hostname}:${port}/`);
+// });
 
-app.listen({ hostname: HOST, port: PORT });
+app.listen({  port: PORT });

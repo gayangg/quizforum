78d7198a0fe810b12e5a1a8ed8b6aa87709cb43d
gayangg <gayang.fi@gmail.com>
2025-01-05 14:36:11 +0200

    Deno.env.get

--- run-locally.js
+++ run-locally.js
@@ -1,7 +1,7 @@
 import { app } from './app.js';
 
 const HOST = '127.0.0.1';
-const PORT = Deno.env.PORT || 7777;
+const PORT = Deno.env.get("PORT") || 7777;
 
 app.addEventListener('listen', ({ hostname, port }) => {
   console.log(`Server running on http://${hostname}:${port}/`);
